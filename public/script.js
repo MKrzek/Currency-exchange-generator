e6e27289a4ee04e3d4d81eba60ac5a7e79543fa1
Malgorzata <mkrzek@googlemail.com>
2017-10-29 22:32:24 +0000

    added styling

--- public/script.js
+++ public/script.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "55c6639a9c427816dc12"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "dd5bdfac56039108ebd2"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
